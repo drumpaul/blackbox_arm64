c282f71e82c71d5769ad31d0682426059aee4f9c
drumpaul <drumpaul@gmail.com>
2015-09-04 21:07:35 +0200

    aggiunti parametri

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+MY_IP=127.0.0.1
 sudo apt-get update
 sudo apt-get -y install ansible
 
 cp /etc/ansible/hosts /etc/ansible/hosts.orig
 echo "[blackbox]" >> /etc/ansible/hosts
-echo "127.0.0.1" >> /etc/ansible/hosts
+echo "$MY_IP" >> /etc/ansible/hosts
 
 ansible-playbook blackbox.yml --connection=local
