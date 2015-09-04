2dd14a7340973c5966293f7268aa814424de3bd5
drumpaul <drumpaul@gmail.com>
2015-09-04 19:50:45 +0200

    aggiunta copia file hosts ansible originale

--- startup.sh
+++ startup.sh
@@ -5,6 +5,7 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 
+cp /etc/ansible/hosts /etc/ansible/hosts.orig
 echo "[blackbox]" >> /etc/ansible/hosts
 echo "127.0.0.1" >> /etc/ansible/hosts
 
