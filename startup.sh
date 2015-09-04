dea9c6848546609e46116a605afb26ba9a5776e4
drumpaul <drumpaul@gmail.com>
2015-09-04 19:34:51 +0200

    Signed-off-by: drumpaul <drumpaul@gmail.com>

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-apt-get update && apt-get -y install ansible
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
 
-echo "[blackbox]\n127.0.0.1" >> /etc/ansible/hosts
+echo "[blackbox]" >> /etc/ansible/hosts
+echo "127.0.0.1" >> /etc/ansible/hosts
+
+ansible-playbook blackbox.yml --connection=local
