fd7050d4f5d895482d1561fbcadf8d295cb1d399
drumpaul <drumpaul@users.noreply.github.com>
2015-10-22 20:44:26 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get -y install git ansible
-git clone git clone https://github.com/drumpaul/blackbox
+git clone https://github.com/drumpaul/blackbox
 cd blackbox
 
 ansible-playbook blackbox.yml --connection=local
