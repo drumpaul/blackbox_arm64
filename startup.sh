41ce1e0cc85b95967dbaa1aaecc90ed7e35fdf82
cotlod <drumpaul@gmail.com>
2015-10-22 20:23:57 +0200

    ottimizzato script startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-MY_IP=127.0.0.1
 sudo apt-get update
-sudo apt-get -y install ansible
-
-cp /etc/ansible/hosts /etc/ansible/hosts.orig
-echo "[blackbox]" >> /etc/ansible/hosts
-echo "$MY_IP" >> /etc/ansible/hosts
+sudo apt-get -y install git ansible
+git clone git clone https://github.com/drumpaul/blackbox
+cd blackbox
 
 ansible-playbook blackbox.yml --connection=local
