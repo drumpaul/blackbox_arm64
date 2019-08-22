654fa05b4f95f6b4cc457dd4071bb3b2b295f0dd
drumpaul <drumpaul@users.noreply.github.com>
2019-08-22 11:28:19 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get -y install git ansible
-git clone https://github.com/drumpaul/blackbox
-cd blackbox
-
-ansible-playbook blackbox.yml -i hosts --connection=local
