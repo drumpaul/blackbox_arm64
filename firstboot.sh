15f6647a8d8112ec0a65205fe7305f2b340bed95
drumpaul <drumpaul@users.noreply.github.com>
2019-08-23 15:30:17 +0200

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "!!!CIAO!!!" > /ciao.txt
 
-#- elimino l'esecuzione dello script per il settaggio del primo avvio
-#sudo ros config set write_files []
+rm -f /tmp/firstboot.sh /etc/rc.local
