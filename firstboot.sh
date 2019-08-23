66e4e1aaf5d312616486a3ceff92f0f7ec17eb11
drumpaul <drumpaul@users.noreply.github.com>
2019-08-23 16:18:43 +0200

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "!!!CIAO!!!" > /ciao.txt
+
+cd /tmp
+
+wget https://github.com/rancher/rancher-compose/releases/download/v0.12.5/rancher-compose-linux-amd64-v0.12.5.tar.gz
+
+tar xvzf rancher-compose-linux-amd64-v0.12.5.tar.gz
+
+mv rancher-compose-v0.12.5/rancher-compose /usr/local/bin/
