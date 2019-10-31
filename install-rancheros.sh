711804b4907af51b39a4dead6815a726abe04735
drumpaul <drumpaul@users.noreply.github.com>
2019-10-31 17:57:00 +0100

    Update install-rancheros.sh

--- install-rancheros.sh
+++ install-rancheros.sh
@@ -1,16 +1,61 @@
 #!/bin/bash
 
+echo "seleziona il profilo di rete tra i seguenti:"
+echo "1) wired - un punto rete LAN"
+echo "2) wireless - un punto rete WLAN"
+echo "3) bridged - 2 punti rete in bridge (1 LAN + 1 WLAN): punto wifi collegato ad AP"
+read profile
+
+case "$profile" in
+	1)
+		echo "$profile"
+		cp cloud-config.template_wired cloud-config.template
+		
+		ip link | awk '{print $2}'
+		echo
+
+		echo "digita il MAC address della scheda da configurare dall'elenco precedente"
+		read iface_wired
+		echo
+		echo $iface_wired
+		echo
+	;;
+	2)
+		cp cloud-config.template_wireless cloud-config.template
+		
+		ip link | awk '{print $2}'
+		echo
+
+		echo "digita il MAC address della scheda da configurare dall'elenco precedente"
+		read iface_wireless
+		echo
+		echo $iface_wireless
+		echo
+	;;
+	3)
+		cp cloud-config.template_bridged cloud-config.template
+		ip link | awk '{print $2}'
+		echo
+
+		echo "digita il MAC address della scheda LAN da configurare dall'elenco precedente"
+		read iface_wired
+		echo
+		echo $iface_wired
+		echo
+		echo "digita il MAC address della scheda WLAN da configurare dall'elenco precedente"
+		read iface_wireless
+		echo
+		echo $iface_wireless
+		echo
+	;;
+	*)
+		echo "ATTENZIONE: nessuna scelta effettuata"
+		exit
+	;;
+esac
+
 src_cloud=cloud-config.template
 dst_cloud=cloud-config.yml
-
-ip link | awk '{print $2}'
-echo
-
-echo "digita il MAC address della scheda da configurare dall'elenco precedente"
-read iface
-echo
-echo $iface
-echo
 
 sudo fdisk -l
 echo
@@ -29,7 +74,8 @@ echo
 
 cp $src_cloud $dst_cloud
 
-sed -i "s/___MACADDRESS___/${iface}/" $dst_cloud
+sed -i "s/___MACADDRESS-WIRED___/${iface_wired}/" $dst_cloud
+sed -i "s/___MACADDRESS-WIRELESS___/${iface_wireless}/" $dst_cloud
 sed -i "s+___PART___+${part}+" $dst_cloud
 
 sudo ros config validate < cloud-config.yml
