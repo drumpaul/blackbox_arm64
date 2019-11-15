09ee1aa1964c7a553773a600444cd3c2fa072850
drumpaul <drumpaul@users.noreply.github.com>
2019-11-15 17:48:07 +0100

    Update run_deluge.sh

--- deluge/run_deluge.sh
+++ deluge/run_deluge.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-docker run --name=backstabbing_pare --env="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" --env="LANG=it_IT.UTF-8" --volume="/home/cotlod:/data" --volume="/data" --expose=53160 -p 8112:8112 --expose=53160/udp --expose=58846 --restart=always --detach=true cotlod/deluge /start.sh
+#DA TESTARE
+#FONTE https://hub.docker.com/r/linuxserver/deluge
+
+docker create \
+  --name=deluge \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=timezone \
+  -e UMASK_SET=022 `#optional` \
+  -e DELUGE_LOGLEVEL=error `#optional` \
+  -v /path/to/deluge/config:/config \
+  -v /path/to/your/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/deluge
