eb598c859e2edc54f8fe3db1d2ff556389e8d2c0
drumpaul <drumpaul@users.noreply.github.com>
2019-11-15 15:01:46 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -2,6 +2,6 @@
 
 git="docker run -ti --rm -v $(pwd):/git bwits/docker-git-alpine"
 clone="clone"
-url="https://github.com/drumpaul/blackbox.git"
+url="https://github.com/drumpaul/blackbox_arm64.git"
 
 $git $clone $url
